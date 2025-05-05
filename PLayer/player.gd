e9fe53ed9d06b90e8c97cfa3c783d7f17d42a3e9
D24125405 <elkhen.gal@gmail.com>
2025-05-06 00:28:04 +0100

    added scenes

--- PLayer/player.gd
+++ PLayer/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
+"res://bullet/bullet.tscn"
+var bullet = preload("res://bullet/bullet.tscn")
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-#@onready var muzzle: Marker2D = $Muzzle
-#var muzzle_offset := Vector2.ZERO
+@onready var muzzle: Marker2D = $Muzzle
+var muzzle_offset := Vector2.ZERO
 
 const GRAVITY = 1000
 const SPEED = 300 
@@ -17,13 +19,19 @@ var shoot_timer := 0.0
 
 func _ready():
 	current_state = State.Idle
-	#muzzle_offset = muzzle.position
+	muzzle_offset = muzzle.position
 
 
 func _physics_process(delta):
-	#player_shooting(delta)
-	#player_muzzle_position()
+	player_shooting(delta)
+	player_muzzle_position()
 
+	if Input.is_action_just_pressed("shoot"):
+		if $Shooting.playing:
+			$Shooting.stop()
+		else:
+			$Shooting.play()
+	queue_redraw()
 	
 	if shoot_timer <= 0:
 		player_falling(delta)
@@ -68,24 +76,24 @@ func player_jump(delta):
 		var direction = Input.get_axis("move_left", "move_right")
 		velocity.x += direction * Jump_horizontal * delta 
 
-#func player_shooting(delta):
-	#if Input.is_action_just_pressed("shoot") and is_on_floor():
-		#var bullet_instance = bullet.instantiate() as Node2D
+func player_shooting(delta):
+	if Input.is_action_just_pressed("shoot") and is_on_floor():
+		var bullet_instance = bullet.instantiate() as Node2D
 		
-		#var shoot_direction = -1 if animated_sprite_2d.flip_h else 1
-		#bullet_instance.direction = shoot_direction
+		var shoot_direction = -1 if animated_sprite_2d.flip_h else 1
+		bullet_instance.direction = shoot_direction
 
-		#bullet_instance.global_position = muzzle.global_position
-		#get_parent().add_child(bullet_instance)
+		bullet_instance.global_position = muzzle.global_position
+		get_parent().add_child(bullet_instance)
 
 		current_state = State.Shoot
 		shoot_timer = 0.3
 		
-#func player_muzzle_position():
-	#if animated_sprite_2d.flip_h:
-	#	muzzle.position.x = -muzzle_offset.x
-	#else:
-	#	muzzle.position.x = muzzle_offset.x
+func player_muzzle_position():
+	if animated_sprite_2d.flip_h:
+		muzzle.position.x = -muzzle_offset.x
+	else:
+		muzzle.position.x = muzzle_offset.x
 
 func player_animations():
 	if current_state == State.Idle:
@@ -96,6 +104,7 @@ func player_animations():
 		animated_sprite_2d.play("Jump")
 	elif current_state == State.Shoot:
 		animated_sprite_2d.play("Shot1")
+
 	#match current_state:
 		#State.Idle:
 		#	animated_sprite_2d.play("Idle")
